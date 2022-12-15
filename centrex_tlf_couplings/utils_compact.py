f62d2307f6a5133ce410e101ae6757a1151919fa
ograsdijk <o.grasdijk@gmail.com>
2022-12-15 17:41:22 -0600

    add functions to compact and add states for the coupling fields

--- centrex_tlf_couplings/utils_compact.py
+++ centrex_tlf_couplings/utils_compact.py
@@ -1,7 +1,10 @@
-from typing import List
+import copy
+from typing import List, Sequence
 
 import numpy as np
 import numpy.typing as npt
+
+from .coupling_matrix import CouplingField, CouplingFields
 
 __all__: List[str] = []
 
@@ -14,6 +17,19 @@ def delete_row_column_indices(
     )
 
 
+def insert_row_column_indices(
+    arr: npt.NDArray[np.float_], indices_insert: npt.NDArray[np.int_]
+) -> npt.NDArray[np.float_]:
+    arr = arr.copy()
+    insert = np.zeros(arr.shape[1], dtype=arr.dtype)
+    for idi in indices_insert:
+        arr = np.insert(arr, idi, insert, axis=0)
+    insert = np.zeros(arr.shape[0], dtype=arr.dtype)
+    for idi in indices_insert:
+        arr = np.insert(arr, idi, insert, axis=1)
+    return arr
+
+
 def BR_to_C_array(BR: npt.NDArray[np.float_], Gamma) -> npt.NDArray[np.float_]:
     return np.sqrt(BR * Gamma)
 
@@ -21,7 +37,7 @@ def BR_to_C_array(BR: npt.NDArray[np.float_], Gamma) -> npt.NDArray[np.float_]:
 def C_array_to_BR(
     C_array: npt.NDArray[np.float_], Gamma: float
 ) -> npt.NDArray[np.float_]:
-    return C_array ** 2 / Gamma
+    return C_array**2 / Gamma
 
 
 def compact_BR_array_indices(
@@ -54,3 +70,42 @@ def compact_C_array_indices(
     BR_compact = compact_BR_array_indices(BR, indices_compact)
     C_array_compact = BR_to_C_array(BR_compact, Gamma)
     return C_array_compact
+
+
+def compact_coupling_field(
+    fields: CouplingFields,
+    indices_compact: Sequence[int],
+) -> CouplingFields:
+    field = []
+    for cf in fields.fields:
+        f = copy.deepcopy(cf.field)
+        f = delete_row_column_indices(f, indices_compact)
+        field.append(CouplingField(polarization=cf.polarization, field=f))
+
+    return CouplingFields(
+        ground_main=fields.ground_main,
+        excited_main=fields.excited_main,
+        main_coupling=fields.main_coupling,
+        ground_states=fields.ground_states,
+        excited_states=fields.excited_states,
+        fields=field,
+    )
+
+
+def insert_levels_coupling_field(
+    fields: CouplingFields,
+    indices_insert: Sequence[int],
+) -> CouplingFields:
+    field = []
+    for cf in fields.fields:
+        f = copy.deepcopy(cf.field)
+        f = insert_row_column_indices(f, indices_insert)
+        field.append(CouplingField(polarization=cf.polarization, field=f))
+    return CouplingFields(
+        ground_main=fields.ground_main,
+        excited_main=fields.excited_main,
+        main_coupling=fields.main_coupling,
+        ground_states=fields.ground_states,
+        excited_states=fields.excited_states,
+        fields=field,
+    )
